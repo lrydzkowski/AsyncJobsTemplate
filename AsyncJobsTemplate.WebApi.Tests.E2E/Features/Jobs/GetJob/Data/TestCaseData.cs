d1463ee303e753a7e315ccc0f251bece2affe7fa
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-11-25 20:02:59 +0100

    feat: add tests for getting job

---
+++ AsyncJobsTemplate.WebApi.Tests.E2E/Features/Jobs/GetJob/Data/TestCaseData.cs
@@ -0,0 +1,12 @@
+using AsyncJobsTemplate.WebApi.Tests.E2E.Common.TestCases;
+
+namespace AsyncJobsTemplate.WebApi.Tests.E2E.Features.Jobs.GetJob.Data;
+
+internal class TestCaseData : ITestCaseData
+{
+    public string JobId { get; init; } = "";
+
+    public int TestCaseId { get; init; }
+
+    public BaseTestCaseData Data { get; init; } = new();
+}
