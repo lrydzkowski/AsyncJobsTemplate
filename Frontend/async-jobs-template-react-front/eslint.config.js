a4b56c46ed642247b390ac1294aac6d8990d8538
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-12-29 14:21:41 +0100

    feat: add react compiler linter

--- Frontend/async-jobs-template-react-front/eslint.config.js
+++ Frontend/async-jobs-template-react-front/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -27,12 +28,14 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       react,
+      'react-compiler': reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
+      'react-compiler/react-compiler': 'error',
     },
   },
 );
