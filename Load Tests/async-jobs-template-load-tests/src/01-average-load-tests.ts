87433b0b9c89e9a62a13454aa6cbc3ea5f204aa6
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-01-06 15:02:42 +0100

    feat: improve example load tests

--- Load Tests/async-jobs-template-load-tests/src/01-average-load-tests.ts
+++ Load Tests/async-jobs-template-load-tests/src/01-average-load-tests.ts
@@ -2,7 +2,7 @@ import { sleep } from 'k6';
 import { Options } from 'k6/options';
 import { host } from './models/test-data.js';
 import { user1AccessToken } from './models/test-data.js';
-import { getJob, getJobs } from './requests.js';
+import { getJob, getJobs, triggerJob } from './requests.js';
 // @ts-expect-error Import module
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 // @ts-expect-error Import module
@@ -23,14 +23,16 @@ export const options: Options = {
 };
 
 export function scenario1() {
-  getJobs(host, user1AccessToken, 1, 5);
-  sleep(randomIntBetween(1, 3) / 0.1);
+  triggerJob(host, 'job1', user1AccessToken);
+  sleep(randomIntBetween(1, 3) * 0.1);
 
-  getJobs(host, user1AccessToken, 2, 5);
-  sleep(randomIntBetween(1, 3) / 0.1);
+  const jobIds = getJobs(host, user1AccessToken, 1, 5);
+  sleep(randomIntBetween(1, 3) * 0.1);
 
-  getJob(host, user1AccessToken, '06f53b5e-cccd-41e3-afaf-e953f540b41a');
-  sleep(randomIntBetween(1, 3) / 0.1);
+  if (jobIds.length > 0) {
+    getJob(host, user1AccessToken, jobIds[0]);
+    sleep(randomIntBetween(1, 3) * 0.1);
+  }
 }
 
 export const handleSummary = function (data: unknown) {
---
+++ Load Tests/async-jobs-template-load-tests/src/models/trigger-job-response.ts
@@ -0,0 +1,3 @@
+export type TriggerJobResponse = {
+  jobId: string;
+};
