1522f867d7acd4d7b2de19344ee0caa5ed801f03
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-01-12 10:08:20 +0100

    feat: add auth to Swagger UI

--- AsyncJobsTemplate.WebApi/ServiceCollectionExtensions.cs
+++ AsyncJobsTemplate.WebApi/ServiceCollectionExtensions.cs
@@ -34,6 +34,34 @@ public static class ServiceCollectionExtensions
             {
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "AsyncJobTemplate", Version = "1.0" });
                 options.EnableAnnotations();
+                options.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Description = "Please enter token",
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "bearer"
+                    }
+                );
+                options.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            []
+                        }
+                    }
+                );
             }
         );
     }
